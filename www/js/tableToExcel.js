964bf1b7e76d65f851dbd13d459791a57fef142a
Arif Budiarto <arifbudi.arto@yahoo.com>
2017-05-04 14:12:01 +0700

    test blobUrl

--- www/js/tableToExcel.js
+++ www/js/tableToExcel.js
@@ -1,3 +1,26 @@
+function b64toBlob(b64Data, contentType, sliceSize) {
+  contentType = contentType || '';
+  sliceSize = sliceSize || 512;
+
+  var byteCharacters = atob(b64Data);
+  var byteArrays = [];
+
+  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    var byteNumbers = new Array(slice.length);
+    for (var i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    var byteArray = new Uint8Array(byteNumbers);
+
+    byteArrays.push(byteArray);
+  }
+    
+  var blob = new Blob(byteArrays, {type: contentType});
+  return blob;
+}
 var tableToExcel = (function() {
   var uri = 'data:application/vnd.ms-excel;base64,'
     , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
@@ -7,6 +30,13 @@ var tableToExcel = (function() {
 	alert("tableToExcel");
     if (!table.nodeType) table = document.getElementById(table)
     var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-    window.location.href = uri + base64(format(template, ctx))
+	var contentType = 'application/vnd.ms-excel';
+	var b64Data = base64(format(template, ctx));
+
+	var blob = b64toBlob(b64Data, contentType);
+	var blobUrl = URL.createObjectURL(blob);
+    //window.location.href = 'http://makruvatech.com?'+uri + base64(format(template, ctx))
+	console.log(blobUrl)
+    window.location.href = blobUrl
   }
 })()
